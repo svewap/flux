1e5bc79520b91c17ad7a22b44fd3b5c7f1906eb3
Sven Wappler <wappler@wappler.systems>
2024-11-11 16:07:04 +0100

    Remove catch of DBALException.

--- Classes/Content/ContentTypeManager.php
+++ Classes/Content/ContentTypeManager.php
@@ -9,7 +9,6 @@ namespace FluidTYPO3\Flux\Content;
  * LICENSE.md file that was distributed with this source code.
  */
 
-use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Driver\Exception;
 use FluidTYPO3\Flux\Content\TypeDefinition\ContentTypeDefinitionInterface;
 use FluidTYPO3\Flux\Content\TypeDefinition\FluidFileBased\DropInContentTypeDefinition;
@@ -68,8 +67,6 @@ class ContentTypeManager implements SingletonInterface
             $this->typeNames = array_merge($this->typeNames, array_keys($types));
             $this->types = $types;
             return $this->types;
-        } catch (DBALException $error) {
-            // Suppress schema- or connection-related issues
         } catch (Exception $error) {
             // Suppress schema- or connection-related issues
         } catch (NoSuchCacheException $error) {
