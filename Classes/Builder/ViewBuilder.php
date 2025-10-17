704c11ff8e91378d0c68e880e4e2c4cc53c7403e
Sven Wappler <wappler@wappler.systems>
2025-10-18 00:45:49 +0200

    fix "PHP Warning: Undefined array key "templateRootPaths" in /.../vendor/fluidtypo3/flux/Classes/Builder/ViewBuilder.php line 129"

--- Classes/Builder/ViewBuilder.php
+++ Classes/Builder/ViewBuilder.php
@@ -126,9 +126,9 @@ class ViewBuilder
         }
 
         if (is_array($extensionKeyOrConfiguration)) {
-            $paths->setTemplateRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_TEMPLATEROOTPATHS]);
-            $paths->setLayoutRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_LAYOUTROOTPATHS]);
-            $paths->setPartialRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_PARTIALROOTPATHS]);
+            $paths->setTemplateRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_TEMPLATEROOTPATHS] ?? []);
+            $paths->setLayoutRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_LAYOUTROOTPATHS] ?? []);
+            $paths->setPartialRootPaths($extensionKeyOrConfiguration[TemplatePaths::CONFIG_PARTIALROOTPATHS] ?? []);
         } else {
             $extensionKey = ExtensionNamingUtility::getExtensionKey($extensionKeyOrConfiguration);
             try {
@@ -214,7 +214,7 @@ class ViewBuilder
 
         return $view;
     }
-    
+
     private function createFluidPathSet(string $extensionKey, string $subPath): array
     {
         return [
